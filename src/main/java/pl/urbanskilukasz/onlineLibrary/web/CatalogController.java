45a5e7e0c555192a87019f6e8b5ee0c759dd71d0
lukaszUrbanski <lukasz.urbanski94@gmail.com>
2022-08-20 22:20:05 +0200

    M4L6 - implementacja tworzenia książki w Rest Api

--- src/main/java/pl/urbanskilukasz/onlineLibrary/web/CatalogController.java
+++ src/main/java/pl/urbanskilukasz/onlineLibrary/web/CatalogController.java
@@ -1,12 +1,15 @@
 package pl.urbanskilukasz.onlineLibrary.web;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.urbanskilukasz.onlineLibrary.catalog.application.port.CatalogUseCase;
+import pl.urbanskilukasz.onlineLibrary.catalog.application.port.CatalogUseCase.CreateBookCommand;
 import pl.urbanskilukasz.onlineLibrary.catalog.domain.Book;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,8 +26,8 @@ public class CatalogController {
     @ResponseStatus(HttpStatus.OK)// default value
     public List<Book> findAll(
             @RequestParam Optional<String> title,
-            @RequestParam Optional<String> author,
-            @RequestParam(defaultValue = "10") int limit
+            @RequestParam Optional<String> author
+           // @RequestParam(defaultValue = "10") int limit
     ){
         if (title.isPresent() && author.isPresent()){
             return catalog.findByAuthorAndTitle(author.get(), title.get()); // todo: resolve problem with searching by title and author
@@ -33,7 +36,8 @@ public class CatalogController {
         }else if (author.isPresent()){
             return catalog.findByAuthor(author.get());
         }
-        return catalog.findAll().stream().limit(limit).collect(Collectors.toList());
+        return catalog.findAll();
+       // return catalog.findAll().stream().limit(limit).collect(Collectors.toList());
     }
 
     @GetMapping(params = {"title"})
@@ -48,5 +52,24 @@ public class CatalogController {
                 .findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createBook(@RequestBody RestCreateBookCommand restCommand){
+        catalog.addBook(restCommand.toCommand());
+    }
+
+    @Data
+    private static class RestCreateBookCommand{
+        private String title;
+        private String author;
+        private Integer year;
+        private BigDecimal price;
+
+        CreateBookCommand toCommand(){
+            return  new CreateBookCommand(title, author, year, price);
+        }
+
     }
 }
