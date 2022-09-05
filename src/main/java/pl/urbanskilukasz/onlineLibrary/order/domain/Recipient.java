2e0b13c95f77b76ff71413b48108c0b73ca6feae
lukaszUrbanski <lukasz.urbanski94@gmail.com>
2022-09-05 22:40:24 +0200

    M5L9 - Dodanie relacji MAny to one dla recipienta

--- src/main/java/pl/urbanskilukasz/onlineLibrary/order/domain/Recipient.java
+++ src/main/java/pl/urbanskilukasz/onlineLibrary/order/domain/Recipient.java
@@ -3,13 +3,19 @@ package pl.urbanskilukasz.onlineLibrary.order.domain;
 import lombok.*;
 
 import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Embeddable
+@Entity
 public class Recipient {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private String phone;
     private String street;
@@ -17,4 +23,13 @@ public class Recipient {
     private String zipCode;
     private String email;
 
+    public Recipient(String name, String phone, String street, String city, String zipCode, String email) {
+
+        this.name = name;
+        this.phone = phone;
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.email = email;
+    }
 }
