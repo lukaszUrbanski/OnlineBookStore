2479e98075fbb4e81009932a3a8e6069aa8618c2
lukaszUrbanski <lukasz.urbanski94@gmail.com>
2022-09-15 21:34:02 +0200

    M7L9 - Dodanie logowania za pomocą @Slf4J

--- src/main/java/pl/urbanskilukasz/onlineLibrary/order/domain/AbandonedOrderJob.java
+++ src/main/java/pl/urbanskilukasz/onlineLibrary/order/domain/AbandonedOrderJob.java
@@ -1,6 +1,7 @@
 package pl.urbanskilukasz.onlineLibrary.order.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import pl.urbanskilukasz.onlineLibrary.order.application.port.ManipulateOrderUseCase;
@@ -11,6 +12,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class AbandonedOrderJob {
@@ -25,7 +27,7 @@ public class AbandonedOrderJob {
         Duration paymentPeriod = properties.getPaymentPeriod();
         LocalDateTime olderThan = LocalDateTime.now().minus(paymentPeriod);
         List<Order> orders = repository.findByStatusAndCreatedAtLessThanEqual(OrderStatus.NEW, olderThan);
-        System.out.println("Found orders to be abandoned :" + orders.size());
+        log.info("Found orders to be abandoned :" + orders.size());
         orders.forEach(order -> orderUseCase.updateOrderStatus(order.getId(), OrderStatus.ABANDONED));
     }
 }
