8b4f7bcb84ad43942897a99ced4ebdb469ccc189
lukaszUrbanski <lukasz.urbanski94@gmail.com>
2022-09-17 22:47:07 +0200

    M8L5 - Przygotowanie danych testowych

--- src/test/java/pl/urbanskilukasz/onlineLibrary/catalog/web/CatalogControllerIT.java
+++ src/test/java/pl/urbanskilukasz/onlineLibrary/catalog/web/CatalogControllerIT.java
@@ -37,46 +37,19 @@ class CatalogControllerIT {
     @Test
     public void getAllBooks() {
         //given
-        Author geatz = authorJpaRepository.save(new Author("Brian Geatz"));
-        Author bloch = authorJpaRepository.save(new Author("Joshua Bloch"));
-        catalogUseCase.addBook(new CreateBookCommand(
-                "Effective Java",
-                Set.of(bloch.getId()),
-                2005,
-                new BigDecimal("99.90"),
-                50L
-        ));
-        catalogUseCase.addBook(new CreateBookCommand(
-                "Java Currency in Practice",
-                Set.of(geatz.getId()),
-                2004,
-                new BigDecimal("129.90"),
-                50L
-        ));
+        givenEffectiveJava();
+        givenJavaCurrencyInPractice();
         //when
         List<Book> all = catalogController.findAll(Optional.empty(), Optional.empty());
         //then
         Assertions.assertEquals(2 , all.size());
     }
+
     @Test
     public void getBookByAuthor() {
         //given
-        Author geatz = authorJpaRepository.save(new Author("Brian Geatz"));
-        Author bloch = authorJpaRepository.save(new Author("Joshua Bloch"));
-        catalogUseCase.addBook(new CreateBookCommand(
-                "Effective Java",
-                Set.of(bloch.getId()),
-                2005,
-                new BigDecimal("99.90"),
-                50L
-        ));
-        catalogUseCase.addBook(new CreateBookCommand(
-                "Java Currency in Practice",
-                Set.of(geatz.getId()),
-                2004,
-                new BigDecimal("129.90"),
-                50L
-        ));
+        givenEffectiveJava();
+        givenJavaCurrencyInPractice();
         //when
         List<Book> all = catalogController.findAll(Optional.empty(), Optional.of("Bloch"));
         //then
@@ -87,7 +60,16 @@ class CatalogControllerIT {
     @Test
     public void getBookByTitle() {
         //given
-        Author geatz = authorJpaRepository.save(new Author("Brian Geatz"));
+        givenEffectiveJava();
+        givenJavaCurrencyInPractice();
+        //when
+        List<Book> all = catalogController.findAll(Optional.of("Java Currency in Practice"), Optional.empty());
+        //then
+        Assertions.assertEquals(1 , all.size());
+        Assertions.assertEquals("Java Currency in Practice", all.get(0).getTitle());
+    }
+
+    private void givenEffectiveJava() {
         Author bloch = authorJpaRepository.save(new Author("Joshua Bloch"));
         catalogUseCase.addBook(new CreateBookCommand(
                 "Effective Java",
@@ -96,6 +78,10 @@ class CatalogControllerIT {
                 new BigDecimal("99.90"),
                 50L
         ));
+    }
+
+    private void givenJavaCurrencyInPractice() {
+        Author geatz = authorJpaRepository.save(new Author("Brian Geatz"));
         catalogUseCase.addBook(new CreateBookCommand(
                 "Java Currency in Practice",
                 Set.of(geatz.getId()),
@@ -103,10 +89,5 @@ class CatalogControllerIT {
                 new BigDecimal("129.90"),
                 50L
         ));
-        //when
-        List<Book> all = catalogController.findAll(Optional.of("Java Currency in Practice"), Optional.empty());
-        //then
-        Assertions.assertEquals(1 , all.size());
-        Assertions.assertEquals("Java Currency in Practice", all.get(0).getTitle());
     }
 }
